94e10c3c9487e73974799014da932277b41393d6
theijhay <olawaleisaacjohn@gmail.com>
2023-11-21 07:50:49 -0800

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 #include <stdbool.h>
-#include <stdio.h>
+
 /**
  * bubble_sort - sorts a doubly linked list of integers in ascending order.
  * using the Insertion sort algorithm.
@@ -8,24 +8,25 @@
  *@size: the size of the array.
 **/
 
-void bubble_sort(int *array, size_t size) 
+void bubble_sort(int *array, size_t size)
 {
-       	unsigned int i, n = size;
+	unsigned int i, n = size;
+	int temp;
 	int new;
 
- do {
+do {
 	new = 0;
-
 	for (i = 1; i < n; i++)
 	{
 	if (array[i - 1] > array[i])
 	{
-		
-	int temp = array[i];
-	array[i] = array[i + 1];
-	array[i + 1] = temp;
-	temp = 1;
+	temp = array[i - 1];
+	array[i - 1] = array[i];
+	array[i] = temp;
+	new = 1;
+	/** print the array after each time you swap two elements **/
+	print_array(array, size);
 	}
 	}
- 	} while (new);
+	} while (new);
 }
